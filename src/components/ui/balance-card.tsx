bf66f78b3fb66cb26d1a0d8584c32d7a3cabbe42
adrianrivald <adrianrivaldy@gmail.com>
2025-07-04 20:12:16 +0700

    fix bg dana pensiun

--- src/components/ui/balance-card.tsx
+++ src/components/ui/balance-card.tsx
@@ -7,41 +7,55 @@ interface BalanceCardProps {
 
 const BalanceCard = ({ balance, percentage }: BalanceCardProps) => {
   return (
-    <Box position="relative">
+    <Box
+      sx={{
+        position: "relative",
+        width: "100%",
+        // height: 160,
+        bgcolor: "#0C3A92",
+        borderRadius: 3,
+        paddingY: 8,
+        paddingX: 4,
+        color: "#ffffff",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        overflow: "hidden",
+      }}
+    >
       <Box
         component="img"
-        src="/images/balance.png"
+        src="/images/balance-1.svg"
         sx={{
-          width: "100%",
-          borderRadius: 3,
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "center",
+          position: "absolute",
+          top: 0,
+          right: 0,
+          width: "15%",
         }}
       />
       <Box
+        component="img"
+        src="/images/balance-2.svg"
         sx={{
           position: "absolute",
-          top: "50%",
-          left: "8%", // or '1rem', adjust as needed
-          transform: "translateY(-50%)", // only vertical centering
-          color: "#ffffff",
+          bottom: 0,
+          left: 0,
+          width: "15%",
         }}
+      />
+      <Typography variant="subtitle2" sx={{ opacity: 0.9 }}>
+        Total Saldo Terkini
+      </Typography>
+      <Typography variant="h5" fontWeight="bold" sx={{ mt: 1 }}>
+        {balance}
+      </Typography>
+      <Typography
+        variant="body2"
+        sx={{ mt: 1, color: "#00e676", fontWeight: 600 }}
       >
-        <Typography variant="subtitle2" sx={{ opacity: 0.9 }}>
-          Total Saldo Terkini
-        </Typography>
-        <Typography variant="h5" fontWeight="bold" sx={{ mt: 1 }}>
-          {balance}
-        </Typography>
-        <Typography
-          variant="body2"
-          sx={{ mt: 1, color: "#00e676", fontWeight: 600 }}
-        >
-          ▲ {percentage}
-          <span style={{ color: "white", fontWeight: 400 }}> bulan lalu</span>
-        </Typography>
-      </Box>
+        ▲ {percentage}
+        <span style={{ color: "white", fontWeight: 400 }}>bulan lalu</span>
+      </Typography>
     </Box>
   );
 };
