14fe6eb3d08bdb76923bc6043bc6dbde14b2ebc4
adrianrivald <adrianrivaldy@gmail.com>
2025-07-07 17:32:26 +0700

    wip: form pencairan dana

--- src/sections/pencairan-dana-pensiun/pencairan-dana-pensiun-view.tsx
+++ src/sections/pencairan-dana-pensiun/pencairan-dana-pensiun-view.tsx
@@ -20,8 +20,8 @@ import { LoadingButton } from "@mui/lab";
 
 export function PencairanDanaPensiunView() {
   const [isSubmitting, setIsSubmitting] = useState(false);
-  const [file, setFile] = useState<File | null>(null);
   const [preview, setPreview] = useState("");
+  const [previewKTP, setPreviewKTP] = useState("");
 
   const handleSubmit = useCallback(async (formData: any) => {
     setIsSubmitting(true);
@@ -55,8 +55,23 @@ export function PencairanDanaPensiunView() {
         //   transition: Bounce,
         // });
       } else if (e.target.files) {
-        setFile(e.target.files[0]);
+        // setFile(e.target.files[0]);
         setPreview(URL.createObjectURL(e.target.files[0]));
+      }
+    }
+  };
+
+  const handleFileKTPChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files) {
+      const size = e.target.files[0]?.size;
+
+      if (size > 5000000) {
+        const reason = `File is larger than ${Math.round(
+          5000000 / 1000000
+        )} mb`;
+      } else if (e.target.files) {
+        // setFile(e.target.files[0]);
+        setPreviewKTP(URL.createObjectURL(e.target.files[0]));
       }
     }
   };
@@ -240,7 +255,8 @@ export function PencairanDanaPensiunView() {
                             component="img"
                             src={preview}
                             sx={{
-                              width: "100%",
+                              width: "45px",
+                              height: "30px",
                               cursor: "pointer",
                               margin: "auto 0",
                             }}
@@ -249,9 +265,11 @@ export function PencairanDanaPensiunView() {
                       </Box>
                       <Stack gap={0}>
                         <Typography fontWeight="bold">Rekening Bank</Typography>
-                        <Typography mt={0}>
-                          Unggah foto buku tabungan
-                        </Typography>
+                        {!preview && (
+                          <Typography mt={0}>
+                            Unggah foto buku tabungan
+                          </Typography>
+                        )}
                       </Stack>
                     </Stack>
                     <Input
@@ -277,13 +295,89 @@ export function PencairanDanaPensiunView() {
                     justifyContent="space-between"
                   >
                     <Stack direction="row" gap={2} alignItems="center">
-                      <Box component="img" src="/images/icons/ktp.svg" />
+                      <Box>
+                        {!previewKTP ? (
+                          <Box component="img" src="/images/icons/wallet.svg" />
+                        ) : (
+                          <Box
+                            component="img"
+                            src={previewKTP}
+                            sx={{
+                              width: "45px",
+                              height: "30px",
+                              cursor: "pointer",
+                              margin: "auto 0",
+                            }}
+                          />
+                        )}
+                      </Box>
                       <Stack gap={0}>
                         <Typography fontWeight="bold">KTP</Typography>
-                        <Typography mt={0}>Unggah KTP</Typography>
+                        {!previewKTP && (
+                          <Typography mt={0}>Unggah KTP</Typography>
+                        )}
                       </Stack>
                     </Stack>
-                    <Box component="img" src="/images/icons/plus.svg" />
+                    <Input
+                      onChange={handleFileKTPChange}
+                      type="file"
+                      sx={{ display: "none" }}
+                      hidden
+                      id="uploadPicKTP"
+                    />
+
+                    <Typography
+                      component="label"
+                      htmlFor="uploadPicKTP"
+                      sx={{ cursor: "pointer", margin: "auto 0" }}
+                    >
+                      <Box component="img" src="/images/icons/plus.svg" />
+                    </Typography>
+                  </Stack>
+                </Stack>
+              </Card>
+
+              <Card
+                sx={{
+                  p: 2,
+                  mt: 4,
+                }}
+              >
+                <Typography fontWeight="bold">Rincian Dana</Typography>
+                <Stack mt={2} gap={2}>
+                  <Stack
+                    direction="row"
+                    justifyContent="space-between"
+                    width="100%"
+                    borderBottom="1px solid #EFF1F5"
+                    pb={2}
+                  >
+                    <Typography sx={{ color: "grey.500" }}>
+                      Total Dana Pensiun
+                    </Typography>
+                    <Typography fontWeight="bold">Rp840.000.000</Typography>
+                  </Stack>
+                  <Stack
+                    direction="row"
+                    justifyContent="space-between"
+                    width="100%"
+                    borderBottom="1px solid #EFF1F5"
+                    pb={2}
+                  >
+                    <Typography sx={{ color: "grey.500" }}>Pajak 5%</Typography>
+                    <Typography fontWeight="bold">Rp5.673.455</Typography>
+                  </Stack>
+                  <Stack
+                    direction="row"
+                    justifyContent="space-between"
+                    width="100%"
+                  >
+                    <Typography sx={{ color: "grey.500", fontWeigh: "bold" }}>
+                      Total Diterima
+                    </Typography>
+                    <Typography fontWeight="bold" sx={{ color: "#19AF66" }}>
+                      Rp845.673.455
+                    </Typography>
                   </Stack>
                 </Stack>
               </Card>
