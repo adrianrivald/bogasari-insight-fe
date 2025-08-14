3df3e2b2e82cabb8f35d320bb4aaa76943546885
adrianrivald <adrianrivaldy@gmail.com>
2025-08-14 12:49:58 +0700

    fix date format

--- src/sections/auth/complete-profile-view.tsx
+++ src/sections/auth/complete-profile-view.tsx
@@ -183,6 +183,7 @@ export function CompleteProfileView() {
                         // error: dateValue === null,
                       },
                     }}
+                    format="DD/MM/YYYY"
                   />
                 </LocalizationProvider>
                 {/* {dateValue === null &&  (
